e9edb9030267324bba0688792c9dbf70b26d6c32
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-06-09 18:40:17 -0500

    📝 add documentation files

---
+++ docs/ejemplo_comentario_documentacion.sql
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ *  © Copyright 2024 MunayWorkers. Todos los derechos reservados.
+ *
+ *  Objetivo:    Retorna lista de colaboradores
+ *
+ *  Fecha Creación:     2024.06.06
+ *  Aplicación:         SAXSAY
+ *
+ *  Autor:              Jheison J. Mayta C.
+ *  Email:              jmayta.caceres@gmail.com
+ *
+ *  Modificaciones:
+ *  Fecha           Autor           Detalle
+ *  2024.06.09      JMC             Se agregó comentario de modificación
+ *
+ *  Ejemplo:
+ *
+    EXEC uspListarColaboradores
+ *
+ ******************************************************************************/
+
+
+/*******************************************************************************
+<?xml version="1.0" encoding="UTF-8"?>
+<documentacion nombre="uspListarColaboradores">
+    <copyright>© Copyright 2024 MunayWorkers. Todos los derechos reservados.</copyright>
+    <objetivo>Retorna lista de colaboradores</objetivo>
+    <fechaCreacion>2024.06.06</fechaCreacion>
+    <modulo>SSB</modulo>
+    <autor>
+        <nombre>Jheison J. Mayta C.</nombre>
+        <email>jmayta.caceres@gmail.com</email>
+    </autor>
+    <modificaciones>
+         <modificacion autorId="jmayta" fecha="2024.06.07">
+            <detalle>Se agregó comentario de modificación</detalle>
+        </modificacion>
+        <modificacion autorId="jmayta" fecha="2024.06.09">
+            <detalle>Se cambió el formato de comentario</detalle>
+        </modificacion>
+    </modificaciones>
+    <ejemplo>
+        <![CDATA[
+            EXEC uspListarColaboradores;
+        ]]>
+    </ejemplo>
+</documentacion>
+*******************************************************************************/
