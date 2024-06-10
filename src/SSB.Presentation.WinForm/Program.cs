85dd2eb8d9f0584e1a53b655bdb94104ad60b8c5
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-06-10 14:40:08 -0500

    🔊 add serilog for console logging

--- src/SSB.Presentation.WinForm/Program.cs
+++ src/SSB.Presentation.WinForm/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SSB.Presentation.WinForm;
 
@@ -11,20 +13,69 @@ static class Program
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default
-        // font, see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
 
         var builder = new ConfigurationBuilder();
         BuildConfig(builder);
 
-        var host = Host.CreateDefaultBuilder()
+        // serilog configuration for passing appsettings.json
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Iniciando Aplicación.");
+
+        try
+        {
+            // configure generic host
+            var host = Host.CreateDefaultBuilder()
+            .UseSerilog()
             .ConfigureServices((context, services) =>
             {
+                // register services
+                services.AddTransient<FrmInicio>();
+            })
+            .Build();
 
-            }).Build();
+            // prevent catching the exceptions when debuggin
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                // Add the event handler for handling UI thread exceptions to the event.
+                Application.ThreadException += (sender, args) =>
+                {
+                    Log.Error(args.Exception, "Excepción de subproceso no controlada.");
+                    MessageBox.Show("Ocurrió un error inesperado.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                };
 
-        Application.Run(new Form1());
+                // Set the unhandled exception mode to force all Windows Forms errors
+                // to go through our handler.
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+                // Add the event handler for handling non-UI thread exceptions to the event. 
+                AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                {
+                    Log.Fatal(args.ExceptionObject as Exception, "Excepción no controlada.");
+                };
+            }
+
+            // To customize application configuration such as set high DPI settings or default
+            // font, see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            var startupForm = host.Services.GetRequiredService<FrmBundler>();
+            Application.Run(startupForm);
+
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "La aplicación terminó inesperadamente");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 
     static void BuildConfig(IConfigurationBuilder builder)
@@ -35,7 +86,6 @@ static class Program
         builder.SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{aspNetCoreEnvironment}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
+            .AddEnvironmentVariables();
     }
 }
