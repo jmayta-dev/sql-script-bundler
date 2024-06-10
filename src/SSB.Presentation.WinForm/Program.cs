994395c88a5726e3d2fbe908cfc1e021fa8b1e26
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-06-09 23:31:36 -0500

    🔧 add config logic for project

--- src/SSB.Presentation.WinForm/Program.cs
+++ src/SSB.Presentation.WinForm/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
 namespace SSB.Presentation.WinForm;
 
 static class Program
@@ -8,9 +11,31 @@ static class Program
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
+        // To customize application configuration such as set high DPI settings or default
+        // font, see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
+
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+
+            }).Build();
+
         Application.Run(new Form1());
-    }    
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        var aspNetCoreEnvironment = Environment.GetEnvironmentVariable(
+            "ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{aspNetCoreEnvironment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+    }
 }
