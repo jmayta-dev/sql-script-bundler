69b3b5d55d1fadd30c3da39e3270d3e6377e4bd6
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-16 22:40:56 -0500

    add domain status enum

---
+++ src/SSB.Domain/Enums/Status.cs
@@ -0,0 +1,14 @@
+namespace SSB.Domain.Enums;
+
+public enum Status
+{
+    Added = 'A',
+    Copied = 'C',
+    Deleted = 'D',
+    Modified = 'M',
+    PairingBroken = 'B',
+    Renamed = 'R',
+    TypeChanged = 'T',
+    Unknown = 'X',
+    Unmerged = 'U'
+}
