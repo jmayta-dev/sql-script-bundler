0d26c26dcfbea80b4182555174bfc8ff395a4f5e
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:44:27 -0500

    add ProcessScripts use case query

---
+++ src/SSB.Application/UseCases/Queries/ProcessScripts/ProcessScriptsScriptDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SSB.Application.UseCases.Queries.ProcessScripts
+{
+    /// <summary>
+    /// Script Data Transfer Objects for processing
+    /// </summary>
+    public record ProcessScriptsScriptDTO
+    {
+        public string? Description { get; set; }
+        public string? FileName { get; set; }
+        public string? Group { get; set; }
+        public string? Observation { get; set; }
+        public string? Path { get; set; }
+        public string? StatusDescription { get; set; }
+    }
+}
