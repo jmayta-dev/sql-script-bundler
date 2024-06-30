35cd74ccf568ce6fbc6926d5794e114f7be5db29
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:43:39 -0500

    add GetAllBranches use case query

---
+++ src/SSB.Application/UseCases/Queries/GetAllBranches/GetAllBranchesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SSB.Shared.Abstractions;
+using System.Collections.Immutable;
+
+namespace SSB.Application.UseCases.Queries.GetAllBranches
+{
+    public record GetAllBranchesQuery(string ProjectPath) :
+        IRequest<Result<IImmutableList<GetAllBranchesBranchDTO>>>;
+}
