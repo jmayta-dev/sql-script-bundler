3a69dee7da4ec9bbbdbb37ac62d6ef6abb7b1774
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:51:42 -0500

    add profile map for dto manipulation

---
+++ src/SSB.Presentation.WinForm/MapProfiles/ScriptDTOProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SSB.Application.UseCases.Commands.MergeScripts;
+using SSB.Application.UseCases.Queries.ProcessScripts;
+
+namespace SSB.Presentation.WinForm.MapProfiles;
+
+public class ScriptDTOProfile : Profile
+{
+    public ScriptDTOProfile()
+    {
+        CreateMap<ProcessScriptsScriptDTO, MergeScriptsScriptDTO>();
+    }
+}
