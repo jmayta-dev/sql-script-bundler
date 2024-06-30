56aaa1ebc2ed10d0cedc5f922a3a1eaef82a767b
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:45:31 -0500

    add MergeScripts use case command

---
+++ src/SSB.Application/UseCases/Commands/MergeScripts/GroupDTO.cs
@@ -0,0 +1,14 @@
+﻿using SSB.Domain.Enums;
+
+namespace SSB.Application.UseCases.Commands.MergeScripts;
+
+/// <summary>
+/// Group Data Transfer Object to store group information and list of
+/// Script Data Transfer Objects for merging process
+/// </summary>
+public record GroupDTO
+{
+    public required string Name { get; init; }
+    public ScriptBundleOperation Operation { get; init; }
+    public List<MergeScriptsScriptDTO> ScriptList { get; set; } = [];
+}
---
+++ src/SSB.Application/UseCases/Commands/MergeScripts/MergeScriptsScriptDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SSB.Application.UseCases.Commands.MergeScripts;
+
+/// <summary>
+/// Script Data Transfer Objects for processing
+/// </summary>
+public record MergeScriptsScriptDTO
+{
+    public required string Path { get; init; }
+    public required string Group { get; init; }
+}
