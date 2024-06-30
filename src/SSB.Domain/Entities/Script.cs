54921394f9489a6a982e1a40fd487f646403c00c
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:28:45 -0500

    🚧 modify domain layer
    + move Error and Result classes to Shared layer
    + rename Status enumerator to ScriptStatus
    + add logic to Status builder and properties
    + add interface for Git service
    + add ScriptBundleOperation enumerator
    + add reference to Shared layer

--- src/SSB.Domain/Entities/Script.cs
+++ src/SSB.Domain/Entities/Script.cs
@@ -1,4 +1,5 @@
 using SSB.Domain.Enums;
+using SSB.Shared.Extensions;
 
 namespace SSB.Domain.Entities;
 
@@ -12,10 +13,19 @@ public sealed class Script
     //
     // public
     //
-    public Status Status { get; init; }
-    public string Group { get; init; }
-    public string Path { get; init; }
-    public IReadOnlyCollection<string> Observations => _observations.AsReadOnly();
+    public required string Path { get; init; }
+    public ScriptStatus Status { get; private set; } = ScriptStatus.Unknown;
+    public string Description { get; init; }
+
+    public string FileName => Path.Split('/').Last();
+    public string Group => Path.Split('/').First();
+    public IReadOnlyList<string> Observations =>
+        _observations
+            .ToHashSet() // removes duplicates
+            .ToList()
+            .AsReadOnly();
+    public string Observation => string.Join(Environment.NewLine, [.. Observations]);
+    public string StatusDescription => Status.GetStringValue() ?? string.Empty;
     #endregion
 
     #region Constructor
@@ -23,39 +33,45 @@ public sealed class Script
     #endregion // Constructor
 
     #region Methods
-    public void AddObservation(string observation)
+    public void AddObservation(string? observation)
     {
-        if (!_observations.Contains(observation))
-            _observations.Add(observation);
+        if (observation == null) return;
+        if (_observations.Contains(observation)) return;
+        _observations.Add(observation);
     }
 
-    public void ClearObservations()
+    public void AddObservation(string[]? observations)
     {
-        _observations.Clear();
+        if (observations == null) return;
+        _observations.AddRange(observations);
     }
+
+    public void ClearObservations() => _observations.Clear();
+
     #endregion // Methods
 
+    /// <summary>
+    /// Script Builder class
+    /// </summary>
     public class ScriptBuilder
     {
         #region Properties & Variables
-        private Status _status;
-        private string _group;
+        private ScriptStatus _status;
+        private string _description;
         private string _path;
+        private readonly List<string> _observations = [];
         #endregion // Properties
 
+        #region Constructor
         private ScriptBuilder() { }
+        #endregion
+
         #region Methods
         public static ScriptBuilder Empty() => new();
 
-        public ScriptBuilder WithStatus(Status status)
+        public ScriptBuilder WithDescription(string description)
         {
-            _status = status;
-            return this;
-        }
-
-        public ScriptBuilder WithGroup(string group)
-        {
-            _group = group;
+            _description = description;
             return this;
         }
 
@@ -64,17 +80,35 @@ public sealed class Script
             _path = path;
             return this;
         }
-        #endregion
+
+        public ScriptBuilder WithStatus(char status)
+        {
+            _status = (ScriptStatus)status;
+            return this;
+        }
+
+        public ScriptBuilder WithStatus(string status)
+        {
+            _status = (ScriptStatus)Convert.ToChar(status);
+            return this;
+        }
+
+        public ScriptBuilder WithStatus(ScriptStatus status)
+        {
+            _status = status;
+            return this;
+        }
 
         public Script Build()
         {
             var _script = new Script
             {
-                Status = _status,
-                Group = _group,
-                Path = _path
+                Description = _description,
+                Path = _path,
+                Status = _status
             };
             return _script;
         }
+        #endregion
     }
 }
