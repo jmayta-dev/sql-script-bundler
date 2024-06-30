f674d2f51ac03bff2f22d0ba965e2435a930e7f4
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-06-30 18:17:38 -0500

    🚧 add shared layer
    + add Error base class for error manipulation
    + add Result base class as alternative to try & catches
    + add StringValue custom attribute to working with enums
    + add method to handle StringValue custom attribute
    + add method "In" for element detecting in collections
    + add static properties and string randomizer

---
+++ src/SSB.Shared/Attributes/StringValueAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace SSB.Shared.Attributes;
+
+/// <summary>
+/// This attribute is used to represent a string value
+/// for a value in an enum.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class StringValueAttribute : Attribute
+{
+    // Attribution to Stefan Sedich's Blog Post:
+    // https://weblogs.asp.net/stefansedich/enum-with-string-values-in-c
+
+    #region Properties
+    public string StringValue { get; protected set; }
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// Constructor used to init a StringValue Attribute
+    /// </summary>
+    /// <param name="value"></param>
+    public StringValueAttribute(string value) => StringValue = value;
+    #endregion
+
+}
